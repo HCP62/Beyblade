93af6585c1647c166e75ddfe226dd7bf1e40e72e
Ameya S <ameyasub@gmail.com>
2024-12-29 23:58:46 -0500

    modified:   test_session.py

--- test_session.py
+++ test_session.py
@@ -18,7 +18,8 @@ class TestSession:
             return 0.0
     
     def start_session(self):
-        for i in range(10):
+        rem = 10
+        while (rem > 0):
             match.increment_battles()
             match = Match(self._bey1, self._bey2)
             print("Enter 1 for your beyblade, 2 for your opponent's beyblade")
@@ -32,6 +33,7 @@ class TestSession:
                 self._bey1.add_win(wt)
             elif (win == 2):
                 self._bey2.add_win(wt)
+            rem -= 1
 
     def calc_wr(self):
         return float(len(self._bey1.get_wins()) / len(self._total_battles)) * 100
